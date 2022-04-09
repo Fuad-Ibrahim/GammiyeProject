c25b839df3dd4d3a35ececcfcec579590f5e9a3a
Fuad-Ibrahim <fuadkile@gmail.com>
2022-04-09 18:37:47 -0500

    Header added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
+import Header from "./components/header";
 
 function App() {
-  return (
-    <>
-      <h1>HEY</h1>
-    </>
-  );
+  return <Header />;
 }
 
 export default App;
---
+++ client/src/components/header.js
@@ -0,0 +1,56 @@
+function Header() {
+  return (
+    <div className="bg-warning">
+      <ul className="nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Home
+          </a>
+        </li>
+        <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            data-bs-toggle="dropdown"
+            href="#"
+            role="button"
+            aria-expanded="false"
+          >
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li>
+              <a className="dropdown-item" href="#">
+                Action
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Another action
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Something else here
+              </a>
+            </li>
+            <li>
+              <hr className="dropdown-divider" />
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Separated link
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
